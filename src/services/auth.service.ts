cbd3a9da6ec2c9f147b13e376bb3e3e2f273a139
bradle254 <leebrad341@gmail.com>
2026-01-18 11:02:09 +0300

    backend

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,27 @@
 import api from './api';
 
-export const login = async (payload: {
-  email: string;
-  password: string;
-}) => {
-  const res = await api.post('/auth/login', payload);
-  return res.data;
-};
+export const AuthService = {
+  login: (data: { email: string; password: string }) => {
+    return api.post('/login', data)
+  },
+  
+  register: (data:{
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+  }) => {
+    return api.post('/register', data)
+  },
+  logout: () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  },
+  get user(){
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+  },
+  isAuthenticated(){
+    return !!localStorage.getItem('token');
+  }
+}
